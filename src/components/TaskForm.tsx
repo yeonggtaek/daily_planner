9b17ad257bc48eecf9c43c9bc4452e97182b0739
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2024-10-24 06:26:32 -0700

    m

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Task.css";
+
+
+export const TaskForm = ({ onSubmit }) => {
+  const [task, setTask] = React.useState("");
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit({
+          id: parseInt((Math.random() * 100).toString(), 10),
+          value: task,
+          completed: false,
+        });
+        setTask("");
+      }}
+    >
+      <input
+        className="add-input"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button className="add-button" type="submit">
+        save
+      </button>
+    </form>
+  );
+};
